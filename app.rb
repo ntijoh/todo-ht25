65cf8fca32077880ce0e42e0ca0516009ee9c4b1
NTIJoshua <joshua.odogwu@elev.ga.ntig.se>
2025-11-27 15:23:23 +0100

    start

--- app.rb
+++ app.rb
@@ -6,10 +6,26 @@ require 'sinatra/reloader'
 
 
 
-
-# Routen /
-get '/' do
-    slim(:index)
+get('/') do
+    slim(:home)
 end
 
+get('/ToDos') do
+    db = SQLITE3::Database.new("db/todos.db")
+    db.results_as_hash = true
+    @datados = db.execute("SELECT * from todos")
+    slim(:"Todos/index")
+end
 
+get('/Categories') do
+    slim(:categories)
+end
+
+post('/Todos/:id/update') do
+    id = params[:id].to_i
+    name = params[:name]
+    description = params[:description]
+    db = SQLITE3::Database.new('db/todos.db')
+    db.execute("UPDATE todos SET name=?,description=? WHERE id=?", [name, description, id])    
+    redirect('/Todos')
+end
